7cb2a7ce910e213c2559581a5efc84b5a0d709d9
Olivier Drieux <olivier.drieux@linkweb.fr>
2025-02-22 19:45:04 +0100

    feat: Add auth middle for dashboard

--- start/routes/web.ts
+++ start/routes/web.ts
@@ -1,6 +1,7 @@
+import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 /* ignore formmating, as I find it easier to scan single line route definitions */
 /* prettier-ignore-start */
 
-router.on('/').renderInertia('home').as('home')
+router.on('/').renderInertia('dashboard').as('dashboard').use(middleware.auth())
