78254caf52dd5758d3b8b39a78b325b83e15881f
Olivier Drieux <olivier.drieux@linkweb.fr>
2025-02-24 11:05:40 +0100

    chore: Some fixes

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -5,7 +5,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LoginController {
     async show({ inertia }: HttpContext) {
-        return inertia.render('login/index')
+        return inertia.render('login/login')
     }
 
     @inject()
